9d8db7372a92812a12ad0a1784e6377aced18210
unknown <ijlnflhbrzdev@gmail.com>
2022-07-21 14:41:31 +0700

    +[UPDATE] Membuat variable bool obs di controller general && menghapus auto initial route

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
               if (snapshot.connectionState == ConnectionState.done) {
                 return GetMaterialApp(
                   title: "Application",
-                  initialRoute: AppPages.INITIAL,
+                  initialRoute: Routes.HOME,
                   getPages: AppPages.routes,
                 );
               }
