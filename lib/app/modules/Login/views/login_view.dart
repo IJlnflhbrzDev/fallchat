5457fa8e8b6a22aaa9020c1087e4fa67bd60ec08
unknown <ijlnflhbrzdev@gmail.com>
2022-07-22 15:07:30 +0700

    +[UPDATE] UI LOGIN PAGE & ADD ASSETS LOGIN

--- lib/app/modules/Login/views/login_view.dart
+++ lib/app/modules/Login/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 import '../controllers/login_controller.dart';
 
@@ -8,14 +9,48 @@ class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Container(
+          height: Get.height,
+          child: Column(
+            children: [
+              Expanded(
+                child: Lottie.asset('assets/login.json'),
+              ),
+              Expanded(
+                flex: 0,
+                child: Container(
+                  width: Get.width * .8,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Color(0XFFDC493C)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(70),
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset('assets/google.png'),
+                        const SizedBox(width: 10),
+                        Text(
+                          'Sign in with Google',
+                          style: TextStyle(
+                            fontSize: 22,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 80),
+            ],
+          ),
         ),
       ),
     );
