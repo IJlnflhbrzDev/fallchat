5b0479a64f867228bf8c11e8eeb3f42659a3436c
unknown <ijlnflhbrzdev@gmail.com>
2022-07-23 09:15:01 +0700

    +[UPDATE] DESIGN UI HOME SCREEN

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,14 +8,61 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
+      floatingActionButton: Container(
+        width: 60,
+        height: 60,
+        child: ElevatedButton(
+            child: Icon(Icons.chat, size: 30),
+            onPressed: () {},
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(1000.0),
+                ),
+              ),
+            )),
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Material(
+              elevation: 5,
+              child: Container(
+                padding: EdgeInsets.fromLTRB(20, 30, 20, 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Chats',
+                      style:
+                          TextStyle(fontSize: 35, fontWeight: FontWeight.w500),
+                    ),
+                    Icon(Icons.person, size: 30.0),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      radius: 25,
+                    ),
+                    title: Text('Orang Ke-$index'),
+                    subtitle: Text(
+                      'Status orang Ke-$index',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    trailing: Chip(
+                      label: Text('3'),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
